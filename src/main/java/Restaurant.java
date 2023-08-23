243e32990d3ddf1697c763592569db36c44f1ad9
pmraje28 <parakhraje28@gmail.com>
2023-08-23 10:22:48 +0530

    Part 3: Solution

--- src/main/java/Restaurant.java
+++ src/main/java/Restaurant.java
@@ -26,25 +26,27 @@ public class Restaurant {
             return false;
     }
 
-    public LocalTime getCurrentTime(){ return  LocalTime.now(); }
+    public LocalTime getCurrentTime() {
+        return LocalTime.now();
+    }
 
     public List<Item> getMenu() {
         return Collections.unmodifiableList(menu);
     }
 
-    private Item findItemByName(String itemName){
-        for(Item item: menu) {
-            if(item.getName().equals(itemName))
+    private Item findItemByName(String itemName) {
+        for (Item item : menu) {
+            if (item.getName().equals(itemName))
                 return item;
         }
         return null;
     }
 
     public void addToMenu(String name, int price) {
-        Item newItem = new Item(name,price);
+        Item newItem = new Item(name, price);
         menu.add(newItem);
     }
-    
+
     public void removeFromMenu(String itemName) throws itemNotFoundException {
 
         Item itemToBeRemoved = findItemByName(itemName);
@@ -53,12 +55,13 @@ public class Restaurant {
 
         menu.remove(itemToBeRemoved);
     }
-    public void displayDetails(){
-        System.out.println("Restaurant:"+ name + "\n"
-                +"Location:"+ location + "\n"
-                +"Opening time:"+ openingTime +"\n"
-                +"Closing time:"+ closingTime +"\n"
-                +"Menu:"+"\n"+getMenu());
+
+    public void displayDetails() {
+        System.out.println("Restaurant:" + name + "\n"
+                + "Location:" + location + "\n"
+                + "Opening time:" + openingTime + "\n"
+                + "Closing time:" + closingTime + "\n"
+                + "Menu:" + "\n" + getMenu());
 
     }
 
@@ -66,4 +69,20 @@ public class Restaurant {
         return name;
     }
 
+    public int calculateOrderCost(List<String> s) {
+        int totalOrderValue = 0;
+        for (String item1 : s) {
+            totalOrderValue = totalOrderValue + getItemPrice(item1);
+        }
+        return totalOrderValue;
+    }
+
+    public int getItemPrice(String str) {
+        for (Item item : menu) {
+            if (item.getName() == str) {
+                return item.getPrice();
+            }
+        }
+        return 0;
+    }
 }
